2d51378a3add0abd94760b1e5984ee7fe258b94f
Greg Moffatt <gmoffatt@users.noreply.github.com>
2017-05-22 08:08:17 -0500

    Add files via upload

---
+++ test/misc/changelog.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="expires" content="0">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>Changelog</title>
+
+		<!-- Bootstrap -->
+		<link href="../css/bootstrap.css" rel="stylesheet">
+		<link href="../css/custom.css" rel="stylesheet">
+		
+		<link href="../tipuesearch/tipuesearch.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<script src="../js/html5shiv.min.js"></script>
+		<script src="../js/respond.min.js"></script>
+	</head>
+	<body>
+	
+<!-- Piwik 
+	Piwik code was here for tracking
+End Piwik Code -->
+
+<div class="container">
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#cpeNavbar" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+	<a href="mailto:email@email.com?Subject=CPE Info Update - Changelog" class="btn btn-default btn-lg btnFix">
+		<span class="glyphicon glyphicon-envelope"></span>
+	</a>
+    </div>
+	
+	<!-- menu php -->
+		<?PHP 
+			include "menu.php"; 
+		?>
+	
+			<div class="row bottom-padding5 topPadding10">
+				<div class="col-lg-8 col-md-8 col-sm-12 topPadding5">
+					<span class="orangePBI">Changelog</span>
+				</div>
+			</div>
+		
+			Complete history of changes to the site:
+				<ul>
+					<li><span>5/28/15:</span> Current updates:</li>
+					<ul>
+						<li>Added <a href="#">Page Title</a></li>
+						<li>Added <a href="#">Page Title</a></li>
+						<li>Added more stuff</li>
+					</ul>
+					<li><span>5/11/15:</span> Current updates:</li>
+					<ul>
+						<li><a href="#">Page Title</a></li>
+					</ul>
+					<li><span>5/8/15:</span> Current updates:</li>
+					<ul>
+						<li>Updated link on <a href="#">Page Title</a></li>
+					</ul>
+					<li><span>4/19/15:</span> Current updates:</li>
+					<ul>
+						<li>Added information</li>
+					</ul>
+					<li><span>4/10/15:</span> Current updates:</li>
+					<ul>
+						<li>Updated link <a href="#">Page Title</a></a></li>
+					</ul>
+					<li><span>4/6/15:</span> Current updates:</li>
+					<ul>
+						<li>Added info on <a href="#">Page Title</a></li>
+						<li>Added <a href="#">Page Title</a></li>
+						<li>Expanded <a href="#">Page Title</a></li>
+						<li>Added info on <a href="#">Page Title</a></li>
+						<li>Updated info on <a href="#">Page Title</a></li>
+					</ul>
+					<li><span>2/13/15:</span> Updates to <a href="#">Page Title</a>:</li>
+						<ul>
+							<li>Update 1</li>
+							<li>Update 2</li>
+							<li>Update 3</li>
+							<li>Update 4</li>
+						</ul>
+					<li><span>2/9/15:</span> Current updates:</li>
+					<ul>
+						<li>Added page for <a href="#">Page Title</a></li>
+						<li>Added page for <a href="#">Page Title</a></li>
+						<li>Added equipment to <a href="#">Page Title</a></li>
+						<li>Added pictures to <a href="#">Page Title</a></li>
+						<li>Updated info on <a href="#">Page Title</a></li>
+					</ul>
+					<li><span>2/3/15:</span> Current updates:</li>
+						<ul>
+							<li>Added <a href="#">Page Title</a></li>
+							<li>Added info</li>
+						</ul>
+					<li><span>1/15/15:</span> Current updates:</li>
+						<ul>
+							<li>Added <a href="#">Page Title</a></li>
+							<li>Added <a href="#">Page Title</a></li>
+							<li>Corrected a broken link on <a href="#">Page Title</a></li>
+							<li>Corrected link on <a href="#">Page Title</a></li>
+							<li>Removed info on <a href="#">Page Title</a></li>
+							<li>Added info</li>
+						</ul>
+					<li><span>12/19/14:</span> Updated return information on <a href="#">Page Title</a>, <a href="#">Page Title</a>, and <a href="#">Page Title</a></li>
+					<li><span>11/29/14:</span> BIG update:</li>
+						<ul>
+							<li>Added <a href="#">Page Title</a>, <a href="#">Page Title</a>, and <a href="#">Page Title</a> pages</li>
+							<li>Updated info</li>
+							<li>Added info</li>
+							<li>Added info on <a href="#">Page Title</a></li>
+							<li>Added pictures to <a href="#">Page Title</a></li>
+							<li>Added info on <a href="#">Page Title</a></li>
+							<li>Updated info on <a href="#">Page Title</a></li>
+						</ul>
+					<li><span>9/30/14:</span> Added info</li>
+					<li><span>9/11/14:</span> Added info</li>
+					<li><span>9/6/14:</span> Added info</li>
+					<li><span>9/4/14:</span> Added info</li>
+					<li><span>8/19/14:</span> Added info</li>
+					<li><span>8/7/14:</span> Added info</li>
+					<li><span>7/31/14:</span> Added info</li>
+					<li><span>7/28/14:</span> Added info</li>
+					<li><span>7/14/14:</span> Added info</li>
+					<li><span>7/10/14:</span> Added info</li>
+					<li><span>7/9/14:</span> Added info</li>
+					<li><span>7/7/14:</span> Added info</li>
+					<li><span>7/7/14:</span> Added info</li>
+				</ul>
+		<div class="footer">
+			<p>Site created by Greg Moffatt</p>
+		</div>
+	</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="../js/jquery-1.11.3.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="../js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ test/misc/menu2.php
@@ -0,0 +1,82 @@
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="cpeNavbar">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="../home.php"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Misc <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="../misc/misc1.php">Miscellaneous1</a></li>
+						<li><a href="../misc/misc2.php">Miscellaneous2</a></li>
+						<li><a href="../misc/misc3.php">Miscellaneous3</a></li>
+						<li><a href="../misc/misc4.php">Miscellaneous4</a></li>
+						<li><a href="../misc/misc5.php">Miscellaneous5</a></li>
+						<li><a href="../misc/misc6.php">Miscellaneous6</a></li>
+						<li><a href="../misc/misc7.php">Miscellaneous7</a></li>
+						<li><a href="../misc/misc8.php">Miscellaneous8</a></li>
+						<li><a href="../misc/misc9.php">Miscellaneous9</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Modem <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a disabled style="font-weight:bold">Manufacturer 1</a></li>
+						<li><a href="../../rg/modem1.php">Modem 1</a></li>
+						<li><a href="../../rg/modem2.php">Modem 2</a></li>
+						<li><a href="../../rg/modem3.php">Modem 3</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a disabled style="font-weight:bold">Manufacturer 2</a></li>
+						<li><a href="../../rg/modem4.php">Modem 4</a></li>
+						<li><a href="../../rg/modem5.php">Modem 5</a></li>
+						<li><a href="../../rg/modem6.php">Modem 6</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a disabled style="font-weight:bold">Manufacturer 3</a></li>
+						<li><a href="../../rg/modem7.php">Modem 7</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">STB <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a disabled style="font-weight:bold">Manufacturer 1</a></li>
+						<li><a href="../../stb/stb1.php">STB 1</a></li>
+						<li><a href="../../stb/stb2.php">STB 2</a></li>
+						<li><a href="../../stb/stb3.php">STB 3</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a disabled style="font-weight:bold">Manufacturer 2</a></li>
+						<li><a href="../../stb/stb4.php">STB 4</a></li>
+						<li><a href="../../stb/stb5.php">STB 5</a></li>
+						<li><a href="../../stb/stb6.php">STB 6</a></li>
+						<li><a href="../../stb/stb7.php">STB 7</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a disabled style="font-weight:bold">Pace</a></li>
+						<li><a href="../../stb/stb8.php">STB 8</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Remote <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a disabled style="font-weight:bold">Standard</a></li>
+						<li><a href="../../remotes/remote1.php">Remote 1</a></li>
+						<li><a href="../../remotes/remote2.php">Remote 2</a></li>
+						<li><a href="../../remotes/remote3.php">Remote 3</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a disabled style="font-weight:bold">Advanced</a></li>
+						<li><a href="../../remotes/remote4.php">Remote 4</a></li>
+						<li><a href="../../remotes/remote5.php">Remote 5</a></li>
+						<li><a href="../../remotes/remote6.php">Remote 6</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">BBU <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="../../bbu/bbu1.php">BBU 1</a></li>
+						<li><a href="../../bbu/bbu2.php">BBU 2</a></li>
+						<li><a href="../../bbu/bbu3.php">BBU 3</a></li>
+						<li><a href="../../bbu/bbu4.php">BBU 4</a></li>
+						<li><a href="../../bbu/bbu5.php">BBU 5</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
